aadb6c505b502bcd3d5d7129b5e76d27a954a7db
Gleb Lugovtsov <lugovtsov.gs@phystech.edu>
2022-03-22 18:25:19 +0300

    Update 4-3 task

--- script4-3.py
+++ script4-3.py
@@ -1,7 +1,9 @@
 import RPi.GPIO as GPIO
 from time import time, sleep
+from math import sin
 
-a = 50
+
+a = 100 * (float(input()) / 3.28)
 frequency = 50
 
 out_pin = 23
@@ -13,9 +15,11 @@ GPIO.setup(out_pin, GPIO.OUT)
 try:
     start = time()
     while True:
+        # a = 50 * (1 + sin(time()))
         if ((start - time()) % (1 / frequency)) < (1 / frequency) * (a / 100):
             GPIO.output(out_pin, 1)
         else:
             GPIO.output(out_pin, 0)
 finally:
+    GPIO.output(out_pin, 0)
     GPIO.cleanup()
